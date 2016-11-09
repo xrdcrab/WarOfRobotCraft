e42294c49fbbc77d519e69a4b2062346ac422748
Ruida Xie (rux793) <rux793@mail.usask.ca>
2016-11-09 16:20:51 -0600

    add HumanPlayer class

---
+++ code/src/model/HumanPlayer.java
@@ -0,0 +1,29 @@
+/*
+ * Name: Ruida Xie
+ * NSID: rux793
+ * Student Number: 11194258
+ */
+package model;
+
+/**
+ * @author ruidaxie
+ *
+ */
+public class HumanPlayer {
+
+	/**
+	 * 
+	 */
+	public HumanPlayer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
