68d38abe66ea78b5e6dacf5a1b8837c123dd3ffd
Jiahuan He (jih889) <jih889@mail.usask.ca>
2016-11-14 16:12:31 -0600

    Add comments of Player class.

--- code/src/model/Player.java
+++ code/src/model/Player.java
@@ -1,7 +1,7 @@
 package model;
 
 /**
- * @author HJH
+ * This is a model class of Player.
  *
  */
 public class Player {
@@ -13,14 +13,23 @@ public class Player {
 	Robot sniperRobot;
 	Robot tankRobot;
 	
+	/**
+	 * this method is to determine whether all the robot are dead. 
+	 */
 	public boolean isDead(){
 		return scoutRobot.isDead() && sniperRobot.isDead() && tankRobot.isDead();
 	}
 	
+	/**
+	 * this method is to get current robot.
+	 */
 	public Robot getCurrentRobot(){
 		return currentRobot;
 	}
 	
+	/**
+	 * this method is to set current robot.
+	 */
 	public void goNextRobot(){
 		if(!(scoutRobot.isHasMoved()&&scoutRobot.isHasShot())){
 			currentRobot = scoutRobot;
@@ -34,6 +43,83 @@ public class Player {
 		else{
 			currentRobot = null;
 		}
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	/**
+	 * @return the scoutRobot
+	 */
+	public Robot getScoutRobot() {
+		return scoutRobot;
+	}
+
+	/**
+	 * @param scoutRobot the scoutRobot to set
+	 */
+	public void setScoutRobot(Robot scoutRobot) {
+		this.scoutRobot = scoutRobot;
+	}
+
+	/**
+	 * @return the sniperRobot
+	 */
+	public Robot getSniperRobot() {
+		return sniperRobot;
+	}
+
+	/**
+	 * @param sniperRobot the sniperRobot to set
+	 */
+	public void setSniperRobot(Robot sniperRobot) {
+		this.sniperRobot = sniperRobot;
+	}
+
+	/**
+	 * @return the tankRobot
+	 */
+	public Robot getTankRobot() {
+		return tankRobot;
+	}
+
+	/**
+	 * @param tankRobot the tankRobot to set
+	 */
+	public void setTankRobot(Robot tankRobot) {
+		this.tankRobot = tankRobot;
+	}
+
+	/**
+	 * @param currentRobot the currentRobot to set
+	 */
+	public void setCurrentRobot(Robot currentRobot) {
+		this.currentRobot = currentRobot;
 	}
 	
 	
