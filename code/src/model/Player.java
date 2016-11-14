99342446c6bea7a82e81c3de2908259c7afc16dd
Jiahuan He (jih889) <jih889@mail.usask.ca>
2016-11-14 16:02:12 -0600

    Implement player class

--- code/src/model/Player.java
+++ code/src/model/Player.java
@@ -1,5 +1,41 @@
 package model;
 
+/**
+ * @author HJH
+ *
+ */
 public class Player {
+	
+	String name;
+	int score;
+	Robot currentRobot;
+	Robot scoutRobot;
+	Robot sniperRobot;
+	Robot tankRobot;
+	
+	public boolean isDead(){
+		return scoutRobot.isDead() && sniperRobot.isDead() && tankRobot.isDead();
+	}
+	
+	public Robot getCurrentRobot(){
+		return currentRobot;
+	}
+	
+	public void goNextRobot(){
+		if(!(scoutRobot.isHasMoved()&&scoutRobot.isHasShot())){
+			currentRobot = scoutRobot;
+		}
+		else if(!(sniperRobot.isHasMoved()&&sniperRobot.isHasShot())){
+			currentRobot = sniperRobot;
+		}
+		else if(!(tankRobot.isHasMoved()&&tankRobot.isHasShot())){
+			currentRobot = tankRobot;
+		}
+		else{
+			currentRobot = null;
+		}
+	}
+	
+	
 
 }
