6d172819ddbfe21c6e4de39b7c1e17d8132eb9d9
Jiahuan He (jih889) <jih889@mail.usask.ca>
2016-11-14 23:26:23 -0600

    Add goNextPlayer()

--- code/src/model/Game.java
+++ code/src/model/Game.java
@@ -17,7 +17,7 @@ public class Game {
 	private int playerNum;
 	
 	/** the current player for the game */
-	private Player currentPlayer;
+	private int currentPlayerIndex;
 	
 	/** the map for the game */
 	private Map gameMap;
@@ -28,12 +28,12 @@ public class Game {
 	 * @param currentPlayer
 	 * @param map
 	 */
-	public Game(HashMap<Integer, Player> playerMap, int playerNum, Player currentPlayer, Map map) {
+	public Game(HashMap<Integer, Player> playerMap, int playerNum, int currentPlayer, Map map) {
 		super();
 		
 		this.playerNum = playerNum;
 		this.playerMap = new HashMap<Integer, Player>();
-		this.currentPlayer = currentPlayer;
+		this.currentPlayerIndex = currentPlayer;
 		this.gameMap = map;
 	}
 	
@@ -48,7 +48,79 @@ public class Game {
 		this.playerNum = playerNum;
 		this.playerMap = new HashMap<Integer, Player>();
 		this.gameMap = new Map(playerNum);
-		this.currentPlayer = null;
+		this.currentPlayerIndex = -1;
 	}
+	
+	/**
+	 * This method is to go next player. 
+	 * @precondition 0<=currentPlayerIndex<5
+	 * @precondition  currentPlayerIndex>=5 or currentPlayerIndex<0
+	 */
+	public void goNextPlayer(){
+		if (this.getCurrentPlayerIndex()<0||this.currentPlayerIndex>=5){
+			this.setCurrentPlayerIndex(0);
+		}
+		else{
+			this.setCurrentPlayerIndex(this.currentPlayerIndex+1);
+		}
+	}
+
+	/**
+	 * @return the playerMap
+	 */
+	public HashMap<Integer, Player> getPlayerMap() {
+		return playerMap;
+	}
+
+	/**
+	 * @param playerMap the playerMap to set
+	 */
+	public void setPlayerMap(HashMap<Integer, Player> playerMap) {
+		this.playerMap = playerMap;
+	}
+
+	/**
+	 * @return the playerNum
+	 */
+	public int getPlayerNum() {
+		return playerNum;
+	}
+
+	/**
+	 * @param playerNum the playerNum to set
+	 */
+	public void setPlayerNum(int playerNum) {
+		this.playerNum = playerNum;
+	}
+
+	/**
+	 * @return the currentPlayerIndex
+	 */
+	public int getCurrentPlayerIndex() {
+		return currentPlayerIndex;
+	}
+
+	/**
+	 * @param currentPlayerIndex the currentPlayerIndex to set
+	 */
+	public void setCurrentPlayerIndex(int currentPlayerIndex) {
+		this.currentPlayerIndex = currentPlayerIndex;
+	}
+
+	/**
+	 * @return the gameMap
+	 */
+	public Map getGameMap() {
+		return gameMap;
+	}
+
+	/**
+	 * @param gameMap the gameMap to set
+	 */
+	public void setGameMap(Map gameMap) {
+		this.gameMap = gameMap;
+	}
+
+	
 	
 }
