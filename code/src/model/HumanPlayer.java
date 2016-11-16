b73305ab3670b57de95cf48ff562b4c21330fc33
Ruida Xie (rux793) <rux793@mail.usask.ca>
2016-11-15 22:09:56 -0600

    add constructor in HumanPlayer class.

--- code/src/model/HumanPlayer.java
+++ code/src/model/HumanPlayer.java
@@ -5,25 +5,28 @@
  */
 package model;
 
+import java.util.LinkedList;
+import model.Map;
+
+
 /**
  * @author ruidaxie
  *
  */
-public class HumanPlayer {
+public class HumanPlayer extends Player{
 
 	/**
-	 * 
+	 * constructor
 	 */
-	public HumanPlayer() {
-		// TODO Auto-generated constructor stub
+	public HumanPlayer (String name, 
+			  			int score, 
+			  			Robot scoutRobot,
+			  			Robot sniperRobot, 
+			  			Robot tankRobot,
+			  			LinkedList<Coordinate> viewRangeList) {
+		super(name, score, scoutRobot, sniperRobot, tankRobot, viewRangeList);
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
 }
