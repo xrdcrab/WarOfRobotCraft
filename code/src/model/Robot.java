4b173b64164e8105ebff23771353ff3ef52f5a6e
Chen Yang (chy202) <chen.y@usask.ca>
2016-11-15 18:17:07 -0600

    modify robot class

--- code/src/model/Robot.java
+++ code/src/model/Robot.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.LinkedList;
+
 import javafx.util.Pair;
 
 /**
@@ -36,6 +38,9 @@ public class Robot {
 	/** robot has shot in a play **/
 	private boolean hasShot;
 	
+	/** robot view range */
+	private LinkedList<Coordinate> viewRangeList;
+	
 	/**
 	 * this constructor is to create a robot with paramaters
 	 * @param type
@@ -54,7 +59,8 @@ public class Robot {
 				Coordinate coord,
 				int direction,
 				boolean hasMoved,
-				boolean hasShot) {
+				boolean hasShot,
+				LinkedList<Coordinate> viewRangeList) {
 		
 		this.name = name;
 		this.attackPoint = attackPoint;
@@ -64,72 +70,153 @@ public class Robot {
 		this.direction = direction;
 		this.hasMoved = hasMoved;
 		this.hasShot = hasShot;
+		this.viewRangeList = viewRangeList;
 	}
 
+
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+
+	/**
+	 * @return the attackPoint
+	 */
 	public int getAttackPoint() {
 		return attackPoint;
 	}
 
+
+	/**
+	 * @param attackPoint the attackPoint to set
+	 */
 	public void setAttackPoint(int attackPoint) {
 		this.attackPoint = attackPoint;
 	}
 
+
+	/**
+	 * @return the healthPoint
+	 */
 	public int getHealthPoint() {
 		return healthPoint;
 	}
 
+
+	/**
+	 * @param healthPoint the healthPoint to set
+	 */
 	public void setHealthPoint(int healthPoint) {
 		this.healthPoint = healthPoint;
 	}
 
+
+	/**
+	 * @return the movementPoint
+	 */
 	public int getMovementPoint() {
 		return movementPoint;
 	}
 
+
+	/**
+	 * @param movementPoint the movementPoint to set
+	 */
 	public void setMovementPoint(int movementPoint) {
 		this.movementPoint = movementPoint;
 	}
 
+
+	/**
+	 * @return the coord
+	 */
 	public Coordinate getCoord() {
 		return coord;
 	}
 
+
+	/**
+	 * @param coord the coord to set
+	 */
 	public void setCoord(Coordinate coord) {
 		this.coord = coord;
 	}
 
+
+	/**
+	 * @return the direction
+	 */
 	public int getDirection() {
 		return direction;
 	}
 
+
+	/**
+	 * @param direction the direction to set
+	 */
 	public void setDirection(int direction) {
 		this.direction = direction;
 	}
-	
-	
+
+
+	/**
+	 * @return the hasMoved
+	 */
 	public boolean isHasMoved() {
 		return hasMoved;
 	}
 
+
+	/**
+	 * @param hasMoved the hasMoved to set
+	 */
 	public void setHasMoved(boolean hasMoved) {
 		this.hasMoved = hasMoved;
 	}
 
+
+	/**
+	 * @return the hasShot
+	 */
 	public boolean isHasShot() {
 		return hasShot;
 	}
 
+
+	/**
+	 * @param hasShot the hasShot to set
+	 */
 	public void setHasShot(boolean hasShot) {
 		this.hasShot = hasShot;
 	}
+
+
+	/**
+	 * @return the viewRangeList
+	 */
+	public LinkedList<Coordinate> getViewRangeList() {
+		return viewRangeList;
+	}
+
+
+	/**
+	 * @param viewRangeList the viewRangeList to set
+	 */
+	public void setViewRangeList(LinkedList<Coordinate> viewRangeList) {
+		this.viewRangeList = viewRangeList;
+	}
+
 
 	/**
 	 * this method is to determine whether the robot is dead
