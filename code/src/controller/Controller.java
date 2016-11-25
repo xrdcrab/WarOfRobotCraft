f5de6cdf070ccb0acec4d8f6108cbeb737aee863
Shisong Wang (shw940) <sean.wong@usask.ca>
2016-11-24 18:12:43 -0600

    delete testing timer and add a timer field and  resetGameBoardViewTimer in Controller

--- code/src/controller/Controller.java
+++ code/src/controller/Controller.java
@@ -32,6 +32,8 @@ public class Controller implements ActionListener, KeyListener {
     private SetGameModeView setGameModeView;
     private GameBoardView gameBoardView;
 
+    private Timer gameBoardViewTimer = new Timer();
+
     //private boolean isMoveMode;
     //private boolean isShootMode;
     //private boolean isTurnMode;
@@ -154,6 +156,26 @@ public class Controller implements ActionListener, KeyListener {
                 }
 
                 return true;
+            }
+
+            private void resetGameBoardViewTimer() {
+                gameBoardViewTimer.cancel();
+                gameBoardViewTimer = new Timer();
+                gameBoardViewTimer.schedule(new TimerTask() {
+
+                    int timerNumber = 30;
+
+                    @Override
+                    public void run() {
+                        if (timerNumber == 0) {
+                            timerNumber = 30;
+                        } else {
+                            timerNumber--;
+                        }
+
+                        gameBoardView.updateTimerNumber(timerNumber);
+                    }
+                }, 0, 1000);
             }
 
             /**
@@ -484,23 +506,6 @@ public class Controller implements ActionListener, KeyListener {
                         System.out.println("player" + i);
                     }
                 }
-
-                // testing timer
-                new Timer().schedule(new TimerTask() {
-
-                    int timerNumber = 10;
-
-                    @Override
-                    public void run() {
-                        if (timerNumber == 0) {
-                            timerNumber = 10;
-                        } else {
-                            timerNumber--;
-                        }
-
-                        gameBoardView.updateTimerNumber(timerNumber);
-                    }
-                }, 0, 1000);
             }
         } // end play button
         else if (e.getSource().equals(this.getGameBoardView().getEndPlayButton())) {
