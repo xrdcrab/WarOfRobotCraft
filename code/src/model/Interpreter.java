1f43c8c5ff350b2f11f17167813d816499312024
Chen Yang (chy202) <chen.y@usask.ca>
2016-11-28 17:43:24 -0600

    add method stringToList() in the interpreter class

--- code/src/model/Interpreter.java
+++ code/src/model/Interpreter.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.LinkedList;
+
 /**
  * this class is to create an interpreter 
  * to transfer the forth script to the java code
@@ -9,5 +11,23 @@ public class Interpreter {
 	
 	public Interpreter () {
 		
+	}
+	
+	/**
+	 * this method is to transfer a string 
+	 * to list of words in that string
+	 * 
+	 * @param forthString the string to be transfered
+	 * @return stringList the linked list of words in that string
+	 */
+	private LinkedList<String> stringToList(String forthString) {
+		LinkedList<String> stringList 
+			= new LinkedList<String>();
+
+		for ( String retriveString: forthString.split(" ") ) {
+			stringList.add( retriveString );
+		}
+		
+		return stringList;
 	}
 }
