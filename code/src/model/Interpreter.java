a14bf40ae7def1b85231f499b188c18ee6284c6f
Chen Yang (chy202) <chen.y@usask.ca>
2016-11-28 17:25:22 -0600

    add interpreter class

---
+++ code/src/model/Interpreter.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * this class is to create an interpreter 
+ * to transfer the forth script to the java code
+ */
+public class Interpreter {
+
+	
+	public Interpreter () {
+		
+	}
+}
