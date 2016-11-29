81c8c0b16febd1a1cc88aa501dac8991d15b9967
Chen Yang (chy202) <chen.y@usask.ca>
2016-11-28 18:12:29 -0600

    add class named Stack for interpreter analyzing forth script

---
+++ code/src/aiutil/Stack.java
@@ -0,0 +1,64 @@
+package aiutil;
+
+import java.util.LinkedList;
+
+public class Stack {
+
+	private LinkedList<String> stack;
+	private String firtElement;
+	private int elementNum;
+	
+	public Stack () {
+		this.stack = new LinkedList<String>();
+		this.firtElement = "";
+		this.elementNum = 0;
+	}
+
+	public void pop () {
+		if ( this.getStack() != null ) {
+			
+		}
+	}
+	
+	/**
+	 * @return the stack
+	 */
+	public LinkedList<String> getStack() {
+		return stack;
+	}
+
+	/**
+	 * @param stack the stack to set
+	 */
+	public void setStack(LinkedList<String> stack) {
+		this.stack = stack;
+	}
+
+	/**
+	 * @return the firtElement
+	 */
+	public String getFirtElement() {
+		return firtElement;
+	}
+
+	/**
+	 * @param firtElement the firtElement to set
+	 */
+	public void setFirtElement(String firtElement) {
+		this.firtElement = firtElement;
+	}
+
+	/**
+	 * @return the elementNum
+	 */
+	public int getElementNum() {
+		return elementNum;
+	}
+
+	/**
+	 * @param elementNum the elementNum to set
+	 */
+	public void setElementNum(int elementNum) {
+		this.elementNum = elementNum;
+	}
+}
