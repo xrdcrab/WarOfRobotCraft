aff6d2a670bc960c782c8074a4da51d3df4d18d5
Shisong Wang (shw940) <sean.wong@usask.ca>
2016-12-08 21:36:08 -0600

    modify AIPlayer class

--- code/src/model/AIPlayer.java
+++ code/src/model/AIPlayer.java
@@ -1,76 +1,108 @@
-
 package model;
 
+import java.util.EventObject;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Vector;
 
 public class AIPlayer extends Player {
 
-	/** all the enemy robots with coordinate */
-	private HashMap<Coordinate, LinkedList<Robot>> enemyMap;
-	
-	/**
-	 * this constructor is to construct the AI player with all parameters
-	 * 
-	 * @param name
-	 * @param score
-	 * @param scoutRobot
-	 * @param sniperRobot
-	 * @param tankRobot
-	 * @param viewRangeList
-	 */
-	public AIPlayer( String name, 
-					int score, 
-					Robot scoutRobot,
-					Robot sniperRobot, 
-					Robot tankRobot,
-					LinkedList<Coordinate> viewRangeList,
-					HashMap<Coordinate, Robot> enemyMap ) {
-		super(name, score, scoutRobot, sniperRobot, tankRobot, viewRangeList);
-		this.enemyMap = new HashMap<Coordinate, LinkedList<Robot>>();
-	}
-	
-	/**
-	 * this constructor is to construct the AI player with 
-	 * initial coordinate and the map size
-	 * 
-	 * @param initialCoord the initial coordinate for the player
-	 * @param mapSize the map size for the game
-	 */
-	public AIPlayer ( Coordinate initialCoord, int mapSize, int playerIndex ) {
-		super ( initialCoord, mapSize, playerIndex );
-		this.enemyMap = new HashMap<Coordinate, LinkedList<Robot>>();
-	}
+    /**
+     * all the enemy robots with coordinate
+     */
+    private HashMap<Coordinate, LinkedList<Robot>> enemyMap;
 
-	/**
-	 * this method is to scan all the coordinates the player can see,
-	 * then store the enemies' robots in the enemyMap.
-	 */
-	public void scan( HashMap<Coordinate, LinkedList<Robot>> robotMap ) {
-		this.enemyMap = new HashMap<Coordinate, LinkedList<Robot>>();
-		for ( Coordinate coord: super.getViewRangeList() ) {
-			if ( robotMap.containsKey(coord) ) {
-				LinkedList<Robot> robotList = new LinkedList<Robot>();				
-				if (robotList.addAll(robotMap.get(coord))) {
-					this.getEnemyMap().put(coord, robotList);
-				};				
-			}
-		}		
-	}
+    /**
+     * this constructor is to construct the AI player with all parameters
+     *
+     * @param name
+     * @param score
+     * @param scoutRobot
+     * @param sniperRobot
+     * @param tankRobot
+     * @param viewRangeList
+     */
+    public AIPlayer(String name,
+            int score,
+            Robot scoutRobot,
+            Robot sniperRobot,
+            Robot tankRobot,
+            LinkedList<Coordinate> viewRangeList,
+            HashMap<Coordinate, Robot> enemyMap) {
+        super(name, score, scoutRobot, sniperRobot, tankRobot, viewRangeList);
+        this.enemyMap = new HashMap<Coordinate, LinkedList<Robot>>();
+    }
 
-	/**
-	 * @return the enemyMap
-	 */
-	public HashMap<Coordinate, LinkedList<Robot>> getEnemyMap() {
-		return enemyMap;
-	}
+    /**
+     * this constructor is to construct the AI player with initial coordinate
+     * and the map size
+     *
+     * @param initialCoord the initial coordinate for the player
+     * @param mapSize the map size for the game
+     */
+    public AIPlayer(Coordinate initialCoord, int mapSize, int playerIndex) {
+        super(initialCoord, mapSize, playerIndex);
+        this.enemyMap = new HashMap<Coordinate, LinkedList<Robot>>();
+    }
 
-	/**
-	 * @param enemyMap the enemyMap to set
-	 */
-	public void setEnemyMap(HashMap<Coordinate, LinkedList<Robot>> enemyMap) {
-		this.enemyMap = enemyMap;
-	}
+    /**
+     * this method is to scan all the coordinates the player can see, then store
+     * the enemies' robots in the enemyMap.
+     */
+    public void scan(HashMap<Coordinate, LinkedList<Robot>> robotMap) {
+        this.enemyMap = new HashMap<Coordinate, LinkedList<Robot>>();
+        for (Coordinate coord : super.getViewRangeList()) {
+            if (robotMap.containsKey(coord)) {
+                LinkedList<Robot> robotList = new LinkedList<Robot>();
+                if (robotList.addAll(robotMap.get(coord))) {
+                    this.getEnemyMap().put(coord, robotList);
+                };
+            }
+        }
+    }
 
+    /**
+     * @return the enemyMap
+     */
+    public HashMap<Coordinate, LinkedList<Robot>> getEnemyMap() {
+        return enemyMap;
+    }
 
+    /**
+     * @param enemyMap the enemyMap to set
+     */
+    public void setEnemyMap(HashMap<Coordinate, LinkedList<Robot>> enemyMap) {
+        this.enemyMap = enemyMap;
+    }
+
+    private Vector vectorListeners = new Vector();
+
+    public synchronized void addAIEventListener(AIEventListener ml) {
+        vectorListeners.addElement(ml);
+    }
+
+    public synchronized void removeAIEventListener(AIEventListener ml) {
+        vectorListeners.removeElement(ml);
+    }
+
+    protected void activateAIEvent(String actionString) {
+        Vector tempVector = null;
+
+        AIEvent e = new AIEvent(this);
+        e.setActionString(actionString);
+
+        synchronized (this) {
+            tempVector = (Vector) vectorListeners.clone();
+
+            for (int i = 0; i < tempVector.size(); i++) {
+                AIEventListener ml = (AIEventListener) tempVector.elementAt(i);
+                ml.EventActivated(e);
+            }
+        }
+
+    }
+
+    public void activateEvent(){
+        activateAIEvent("");
+    }
 }
